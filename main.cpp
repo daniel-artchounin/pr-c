d33ed3896de78ca694bf833d3a5fa3064927ce42
daniel <daniel-artchounin@users.noreply.github.com>
2015-05-17 23:46:25 +0200

    suppression : main.cpp + pr-c.pro

--- main.cpp
+++
@@ -1,9 +0,0 @@
-# include <QApplication>
-# include <QPushButton>
-
-int main(int argc, char * argv[]){
-    QApplication myApp(argc, argv);
-    QPushButton bouton("Bonjour");
-    bouton.show();
-    return myApp.exec();
-}
