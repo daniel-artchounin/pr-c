9a1f2318dd7762a23394a524c988c4c04422e0d5
Kapilraj Thangeswaran <tkapilraj@gmail.com>
2015-05-17 15:45:34 +0200

    Ajout classe ObjetAvecDebut

---
+++ objetavecdebut.h
@@ -0,0 +1,37 @@
+#ifndef OBJETAVECDEBUT_H
+#define OBJETAVECDEBUT_H
+#include "date.h"
+#include "horaire.h"
+
+/*! \class ObjetAvecDebut
+   * \brief Classe possédant une date et un horaire de début
+   */
+class ObjetAvecDebut
+{
+private:
+    Date dateDebut; /*!< date de début */
+    Horaire horaireDebut; /*!< horaire de début */
+public:
+    /*!
+     * \brief Constructeur
+     * \param d date de début
+     * \param h horaire de début
+     */
+    ObjetAvecDebut(const Date& d, const Horaire& h):dateDebut(d),horaireDebut(h) {}
+    /*!
+     * \brief getDateDebut
+     * \return date de début
+     */
+    const Date& getDateDebut() const {
+        return dateDebut;
+    }
+    /*!
+     * \brief getHoraireDebut
+     * \return horaire de début
+     */
+    const Horaire& getHoraireDebut() const {
+        return horaireDebut;
+    }
+};
+
+#endif // OBJETAVECDEBUT_H
