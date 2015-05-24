8d81c9aaae11d18e68c58ff84e84a73f01463e47
Kapilraj Thangeswaran <tkapilraj@gmail.com>
2015-05-24 18:59:54 +0200

    Ajout classe ProgrammationManagerException

---
+++ programmationmanagerexception.h
@@ -0,0 +1,21 @@
+#ifndef PROGRAMMATIONMANAGEREXCEPTION
+#define PROGRAMMATIONMANAGEREXCEPTION
+
+#include <stdexcept>
+
+/*!
+ * \class ProgrammationManagerException
+ * Permet de générer des exception pour les ProgrammationManagers
+ * Hérite de la classe Logic_error
+ */
+class ProgrammationManagerException : public std::logic_error {
+public:
+    /*!
+     * \brief Constructeur
+     * \param m information sur l'exception
+     */
+    ProgrammationManagerException(const std::string& m):logic_error(m) {}
+};
+
+#endif // PROGRAMMATIONMANAGEREXCEPTION
+
