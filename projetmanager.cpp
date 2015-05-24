9d104a6ab4808390a907405860367667a292ae91
Kapilraj Thangeswaran <tkapilraj@gmail.com>
2015-05-24 14:32:09 +0200

    Modification de la classe ProjetManager

--- projetmanager.cpp
+++ projetmanager.cpp
@@ -1,9 +1,5 @@
 #include "projetmanager.h"
 #include "projetmanagerexception.h"
-#include "date.h"
-#include "horaire.h"
-#include "duree.h"
-#include <string>
 
 //design pattern singleton
 ProjetManager * ProjetManager::instance=0; //initialisation à null, pour la première vérification
@@ -18,55 +14,20 @@ void ProjetManager::libererInstance() {
     if(!instance) delete instance;
 }
 
-void ProjetManager::addItem(Projet* projet) {
-    if (nb==nbMax) {
-        Projet** newtab=new Projet*[nbMax+10];
-        for(unsigned int i=0; i<nb; i++) newtab[i]=projets[i];
-        nbMax+=10;
-        Projet** old=projets;
-        projets=newtab;
-        delete[] old;
+Projet& ProjetManager::addProjet(const std::string titre, const Date dateDebut, const Horaire horaireDebut, const Date dateFin, const Horaire horaireFin) {
+    Projet* projet = new Projet(dateDebut, horaireDebut, dateFin, horaireFin, titre);
+    if(!Manager::addItem(titre, projet)) {
+        throw ProjetManagerException("Erreur, ProjetManager, addProjet, Projet deja existant");
     }
-    projets[nb++]=projet;
+    return *projet;
 }
 
-Projet* ProjetManager::trouverProjet(const std::string& titre)const {
-    for(unsigned int i=0; i<nb; i++)
-        if (titre==projets[i]->getTitre()) return projets[i];
-    return 0;
-}
-
-Projet& ProjetManager::ajouterProjet(const Date& dateDebut, const Horaire& horaireDebut, const Date& dateFin, const Horaire& horaireFin, const std::string& titre) {
-    if (trouverProjet(titre)) throw ProjetManagerException("erreur, ProjetManager, Projet deja existante");
-    Projet* newt=new Projet(dateDebut,horaireDebut,dateFin,horaireFin,titre);
-    addItem(newt);
-    return *newt;
-}
-
-Projet& ProjetManager::getProjet(const std::string& id) {
-    Projet* t=trouverProjet(id);
-    if (!t) throw ProjetManagerException("erreur, ProjetManager, Projet inexistante");
-    return *t;
+Projet& ProjetManager::getProjet(const std::string& titre) {
+    Projet* projet=getItem(titre);
+    if (!projet) throw ProjetManagerException("erreur, ProjetManager, getProjet, Projet inexistant");
+    return *projet;
 }
 
 const Projet& ProjetManager::getProjet(const std::string& id)const {
     return const_cast<ProjetManager*>(this)->getProjet(id);
 }
-
-ProjetManager::~ProjetManager() {
-    for(unsigned int i=0; i<nb; i++) delete projets[i];
-    delete[] projets;
-}
-
-/*
-ProjetManager::ProjetManager(const ProjetManager& pm) : projets(new Projet*[pm.nb]), nb(pm.nb), nbMax(pm.nbMax) {
-    for(unsigned int i=0; i<nb; i++) projets[i]=new Projet(*pm.projets[i]);
-}
-
-ProjetManager& ProjetManager::operator=(const ProjetManager& pm) {
-    if (this==&pm) return *this;
-    this->~ProjetManager();
-    projets=pm.projets;
-    return *this;
-}
-*/
--- projetmanager.h
+++ projetmanager.h
@@ -1,82 +1,79 @@
 #ifndef PROJETMANAGER_H
 #define PROJETMANAGER_H
+
+#include "manager.h"
 #include "projet.h"
-#include <vector>
+#include "date.h"
+#include "horaire.h"
 #include <string>
 
-class ProjetManager {
+/*! \class ProjetManager
+ * \brief Classe permettant de manipuler des projets. Hérite de la classe Manager. Utilise le design pattern singleton
+ */
+class ProjetManager : public Manager<Projet> {
 private:
-    Projet** projets;
-    unsigned int nb;
-    unsigned int nbMax;
-    void addItem(Projet* p);
-    Projet* trouverProjet(const std::string& titre) const;
-
     //design pattern singleton
-    static ProjetManager * instance;
+    static ProjetManager * instance; /*!< instance unique de ProjetManager */
 
-    ProjetManager(): projets(0), nb(0), nbMax(0){}
-    ~ProjetManager();
+    /*!
+     * \brief Constructeur
+     * Constructeur privé afin d'empêcher la duplication
+     */
+    ProjetManager(): Manager<Projet>(){}
+    /*!
+     *\brief Destructeur
+     * Destructeur privé
+     */
+    ~ProjetManager() {}
+    /*!
+     * \brief Contructeur par recopie
+     * Constructeur par recopie privé
+     * \param pm ProjetManager
+     */
     ProjetManager(const ProjetManager& pm);
+    /*!
+     * \brief Operateur d'affectation
+     * Opérateur d'affectation privé
+     * \param pm ProjetManager
+     */
     ProjetManager& operator=(const ProjetManager& pm);
 public:
-    Projet& ajouterProjet(const Date& dateDebut, const Horaire& horaireDebut, const Date& dateFin, const Horaire& horaireFin, const std::string& titre);
-    Projet& getProjet(const std::string& titre);
-    const Projet& getProjet(const std::string& titre) const;
-
     //design pattern singleton
+    /*!
+     * \brief getInstance
+     * \return instance unique de ProjetManager
+     */
     static ProjetManager& getInstance();
+    /*!
+     * \brief libererInstance
+     * Détruit l'instance unique de ProjetManager
+     */
     static void libererInstance();
-
-    //Iterator méthode STL
-    class iterator {
-        Projet ** current;
-        iterator(Projet ** projet) : current(projet) {}
-        friend class ProjetManager;
-    public :
-        iterator():current(0) {}
-        bool operator!=(const iterator & i) const {
-            return i.current != this->current;
-        }
-        iterator & operator++(int) {
-            ++current;
-            return *this;
-        }
-        Projet & operator*() const {
-            return **current;
-        }
-    };
-    iterator begin() {
-        return iterator(projets);
-    }
-    iterator end() {
-        return iterator(projets+nb);
-    }
-
-    //iterator const
-    class const_iterator {
-        Projet ** current;
-        const_iterator(Projet ** projet) : current(projet) {}
-        friend class ProjetManager;
-    public :
-        const_iterator():current(0) {}
-        bool operator!=(const const_iterator & i) const {
-            return i.current != this->current;
-        }
-        const_iterator & operator++(int) {
-            ++current;
-            return *this;
-        }
-        Projet & operator*() const {
-            return **current;
-        }
-    };
-    const_iterator begin() const {
-        return const_iterator(projets);
-    }
-    const_iterator end() const {
-        return const_iterator(projets+nb);
-    }
+    /*!
+     * \brief addProjet
+     * Génère une exception ProjetManagerException si un projet ayant le même titre existe déjà
+     * \param titre titre du projet
+     * \param dateDebut date de début du projet
+     * \param horaireDebut horaire de début du projet
+     * \param dateFin date de fin du projet
+     * \param horaireFin horaire de fin du projet
+     * \return Projet si celui-ci a été ajouté
+     */
+    Projet& addProjet(const std::string titre, const Date dateDebut, const Horaire horaireDebut, const Date dateFin, const Horaire horaireFin);
+    /*!
+     * \brief getProjet
+     * Génère une exception ProjetManagerException si le projet n'a pas été trouvé
+     * \param titre titre du projet à trouver
+     * \return Projet si le projet existe
+     */
+    Projet& getProjet(const std::string& titre);
+    /*!
+     * \brief getProjet
+     * Génère une exception ProjetManagerException si le projet n'a pas été trouvé
+     * \param titre titre du projet à trouver
+     * \return const Projet si le projet existe
+     */
+    const Projet& getProjet(const std::string& titre) const;
 };
 
 #endif // PROJETMANAGER_H
