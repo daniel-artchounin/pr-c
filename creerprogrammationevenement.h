d8e19fa84faff1d96c4d420b9dd8617deff6fb5f
Kapilraj Thangeswaran <tkapilraj@gmail.com>
2015-06-03 09:55:51 +0200

    Ajout formulaire CreerProgrammationEvenement

---
+++ creerprogrammationevenement.h
@@ -0,0 +1,39 @@
+#ifndef CREERPROGRAMMATIONEVENEMENT_H
+#define CREERPROGRAMMATIONEVENEMENT_H
+
+#include <QWidget>
+#include <QFormLayout>
+#include <QLineEdit>
+#include <QDateEdit>
+#include <QTimeEdit>
+#include <QSpinBox>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+
+class CreerProgrammationEvenement: public QWidget
+{
+    Q_OBJECT
+protected :
+
+private :
+    QFormLayout* formlayout;
+    QLineEdit* nom; /*!< nom de l'évènement */
+    QDateEdit* dateDebut;/*!< date de début */
+    QTimeEdit* horaireDebut; /*!< horaire de début */
+    QSpinBox* duree; /*!< duree */
+    QPushButton* valider;  /*!< bouton valider */
+    QPushButton* annuler;  /*!< bouton annuler */
+    QHBoxLayout* hBox;
+    QVBoxLayout* vBox;
+public:
+    explicit CreerProgrammationEvenement(QWidget *parent = 0);
+
+signals:
+
+public slots:
+    void closeEvent(QCloseEvent *event);
+    void programmer();
+};
+
+#endif // CREERPROGRAMMATIONEVENEMENT_H
